cb6b7ebf90c937d26dcb7955a364c747a8644479
Okosodo Norah <ksdnorah@gmail.com>
2024-03-11 13:29:41 -0700

    commit

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,11 @@
+// utils.js
+
+const uploadPhoto = () => {
+  // Implementation for uploadPhoto
+};
+
+const createUser = () => {
+  // Implementation for createUser
+};
+
+module.exports = { uploadPhoto, createUser };
